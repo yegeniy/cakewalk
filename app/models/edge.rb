bd1d2f711ee9f57cc036d8d74fb7d2aed356e09b
Your Name <evulfson@gmail.com>
2010-06-16 15:24:16 -0400

    displays, but does not add edges in add_new_path

--- app/models/edge.rb
+++ app/models/edge.rb
@@ -1,6 +1,6 @@
 class Edge < ActiveRecord::Base
   belongs_to :point, :class_name => "Point"
   belongs_to :to_point, :class_name => "Point"
-  validates_presence_of :point
+  validates_presence_of :to_point
     
 end
