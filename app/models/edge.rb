0080d17d3c3fe328e10e6cc9c3e40251184e3d0e
Your Name <evulfson@gmail.com>
2010-06-19 14:12:19 -0400

    adding collection select instead of radio buttons

--- app/models/edge.rb
+++ app/models/edge.rb
@@ -2,6 +2,7 @@ class Edge < ActiveRecord::Base
 
   belongs_to :path, :class_name => "Path"
   belongs_to :point, :class_name => "Point"
+  belongs_to :endpoint, :class_name => "Point"
 
 #  belongs_to :to_point, :class_name => "Point"
   validates_presence_of :point, :endpoint
