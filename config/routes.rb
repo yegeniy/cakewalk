6c208a5878934d8c4f2a5f4b9666ed912fe12fdd
Nick Gordon <nickggordon@gmail.com>
2010-06-20 16:51:38 -0400

    adding search

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.connect 'search/map', :controller => 'search', :action => "map", :method => :get
+  map.connect 'search/getdirect', :controller => 'search', :action => "getdirect"
   map.resources :paths
   map.resources :users
   map.resources :edges
@@ -42,6 +44,7 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
+  
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
