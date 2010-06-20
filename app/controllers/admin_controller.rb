f6e5ed25654b2bfc81451b50d1c044ff28f8722f
Theodore Pon <theopon@brandeis.edu>
2010-06-20 15:05:50 -0400

    Home page stuffs/views/css

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,5 @@
 class AdminController < ApplicationController
-
+layout "app"
   # just display the form and wait for user to
   # enter a name and password
 
@@ -16,6 +16,7 @@ before_filter :requirelogin, :except => ["login", "logout"]
           redirect_to(:action => "welcome")
         else
           flash.now[:notice] = "You are not an administrator..." 
+          redirect_to(:controller => "home", :action => "index")
         end   
       else
         flash.now[:notice] = "Invalid user/password combination"
