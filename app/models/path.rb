74df2a2d7ee0adab85850c20899619a94d78bab0
Your Name <evulfson@gmail.com>
2010-06-21 23:28:44 -0400

    added remove checkbox to path edges field

--- app/models/path.rb
+++ app/models/path.rb
@@ -3,7 +3,7 @@ class Path < ActiveRecord::Base
 #  has_many :legs, :through => :edges, :source => :point
   has_many :points, :through => :edges
 
-  accepts_nested_attributes_for :edges
+  accepts_nested_attributes_for :edges, :allow_destroy => true
 =begin
   def edge_attributes=(edge_attributes)
     edge_attributes.each do |attributes|
