82d9f2f79d4688b17e55466d68cebd045028f982
Theodore Pon <theopon@brandeis.edu>
2010-06-29 23:28:59 -0400

    mobile stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
 #  include SslRequirement
   helper :all # include all helpers, all the time
-
+  has_mobile_fu
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery :secret => '8fc080370e56e929a2d5afca5540a0f7'
-  
+ # before_filter :prepare_for_mobile
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
@@ -28,5 +28,19 @@ protected
       flash[:notice]= "Please log in first.."
       redirect_to :controller => 'admin', :action => 'login'
   end    
+  
+  def mobile_device?
+  	if session[:mobile_param]
+  		session[:mobile_param] == "1"
+  	else
+  		request.user_agent =~ /Mobile|webOS/
+  	end
+  end
+  helper_method :mobile_device?
+  
+  def prepare_for_mobile
+  	session[:mobile_param]= params[:mobile] if params[:mobile]
+  	request.format = :mobile if mobile_device?
+  end				
    
 end
