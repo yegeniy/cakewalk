9780c185a36d61b2198bc98e6384287c144d5709
Nick Gordon <nickggordon@gmail.com>
2010-07-01 15:11:14 -0400

    modified:   app/controllers/home_controller.rb 	modified:   app/views/home/index.html.erb 	modified:   app/views/layouts/application.html.erb
    adding some small things to the home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
+  def index
+    @point_names = []
+    @points = Point.all
+    @points.each { |point| @point_names << point.name }
+    puts @points
+  end
 
-def authorize
-end
+  def authorize
+  end
 end
