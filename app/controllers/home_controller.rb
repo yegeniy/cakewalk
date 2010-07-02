0d532925900eb4e826829d10e3782eb34e3b58dd
Nick Gordon <nickggordon@gmail.com>
2010-07-01 23:19:06 -0400

    modified:   app/controllers/home_controller.rb 	modified:   app/views/home/index.html.erb 	new file:   public/images/ui-anim_basic_16x16.gif 	new file:   public/javascripts/jquery-1.4.2.min.js 	new file:   public/javascripts/jquery-ui-1.8.2.custom.min.js 	modified:   public/stylesheets/application.css 	new file:   public/stylesheets/jquery-ui-1.8.2.custom.css auto-complete added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     @point_names = []
     @points = Point.all
-    @points.each { |point| @point_names << point.name }
+    @points.each { |point| if point.name then @point_names << point.name end }
     puts @points
   end
 
